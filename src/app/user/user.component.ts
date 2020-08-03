37467b9cf31c74e1270e6f4d119fd8e97af51e59
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-08-03 14:40:52 -0400

    basic event emitter design pattern here, will do a subject pattern next

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { UserService } from "../user.service";
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -9,7 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private userService: UserService) {
   }
 
   ngOnInit() {
@@ -18,5 +18,10 @@ export class UserComponent implements OnInit {
       {
       this.id = +params.id;
     });
+  }
+
+
+  onActivate(){
+    this.userService.activatedEmitter.emit(true);
   }
 }
